ae3d47dcc40b72f5426b8bfd4402ea1875f2398f
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-19 23:27:31 +0600

    new components create

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAf4irP_KpbafR0mGbe_mnVlzsxfS4Y-Xc",
+  authDomain: "guestbook-db4db.firebaseapp.com",
+  projectId: "guestbook-db4db",
+  storageBucket: "guestbook-db4db.appspot.com",
+  messagingSenderId: "243942582557",
+  appId: "1:243942582557:web:59f2257d48acee75a059f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
