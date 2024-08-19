2ef4db1a7b78634ce2b927838124d026c893aec2
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-20 01:50:47 +0600

    now working register

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; //using for email and password
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +13,24 @@ const firebaseConfig = {
   projectId: "guestbook-db4db",
   storageBucket: "guestbook-db4db.appspot.com",
   messagingSenderId: "243942582557",
-  appId: "1:243942582557:web:59f2257d48acee75a059f4"
+  appId: "1:243942582557:web:59f2257d48acee75a059f4",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); //using for email and password
+
+//using for registration
+const registerWithEmailAndPassword = async (email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    // console.log(user);
+    return user;
+  } catch (err) {
+    // console.log(error);
+    throw (error);
+  }
+};
+
+export { registerWithEmailAndPassword };
